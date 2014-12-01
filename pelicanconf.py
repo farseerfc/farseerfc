41f7c7cd176edda2c2292d488b3b7abfb2e7cf4a
Jiachen Yang <jc-yang@ist.osaka-u.ac.jp>
2014-12-01 22:38:06 +0900

    changed icon for lilydjwg

--- pelicanconf.py
+++ pelicanconf.py
@@ -28,7 +28,7 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 # Blogroll
 LINKS = (('lilydjwg',
           'http://lilydjwg.is-programmer.com/', 
-          'https://zh.wikipedia.org/wiki/File:Vulpes_vulpes_laying_in_snow.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/0/03/Vulpes_vulpes_laying_in_snow.jpg',
           '依雲（aka. 仙子百合）'),
          ('felixonmars',
           'http://blog.felixc.at/',
